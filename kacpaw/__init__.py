b0f3163c8e2fde2b409d10fe8b67ca971f07d02a
Potato42 <bburrill98@gmail.com>
2016-01-09 19:12:18 -0800

    made some changes to __init__.py

--- kacpaw/__init__.py
+++ kacpaw/__init__.py
@@ -1,24 +1,7 @@
-# KACPAW - KA CS API Wrapper
-# An interface for pefrorming user actions related to the Computer Science environment on Khan Academy
-
-## Every time I run the program it puts its source inside the source of a KA program.
-
-# KACPAW currently has pretty bad documentation, and a lot of things are left undocumented.  However, if you scroll down to ``if __name__ == "__main__"``, you can get a feel for how it works.
-# todo: better/more documentation
-
-#import requests
-
-
-
+"""
+KACPAW
+"""
 
 from .content import *
 from .sessions import *
-
-
-
-
-
-
-
-## tests.  Ok, so this isn't exactly automated testing.  I just run a bunch of stuff and make sure the output makes sense and there are no errors.
-#if __name__ == "__main__": # todo: move/improve
+import kacpaw.content_abcs as abcs
